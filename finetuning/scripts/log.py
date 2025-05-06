fdcb90824ef78104b2a372b53f2cfefd68b90a55
Yohannes Taye <ytaye715@gmail.com>
2025-05-06 18:38:49 +0300

    update

--- finetuning/scripts/log.py
+++ finetuning/scripts/log.py
@@ -1,12 +1,12 @@
 import logging
 
-def get_file_logger(log_file="training.log"):
+def get_file_logger(log_file="finetuning/training.log"):
     logger = logging.getLogger("TrainingLogger")
     logger.setLevel(logging.INFO)
 
-    # Avoid adding multiple handlers if get_file_logger is called multiple times
     if not logger.handlers:
-        file_handler = logging.FileHandler(log_file)
+        # Add encoding="utf-8" to the FileHandler
+        file_handler = logging.FileHandler(log_file, encoding="utf-8")
         formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
         file_handler.setFormatter(formatter)
         logger.addHandler(file_handler)
